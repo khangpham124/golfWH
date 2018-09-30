e07eceb76efb307cbf277a3be9505339574d0ff8
khangpham <khangpham421@gmail.com>
2018-09-30 11:29:25 +0700

    header done

--- libs/header.php
+++ libs/header.php
@@ -14,14 +14,14 @@
                 <li class="active"><a href="">ENG</a></li>
                 <li><a href="">日本語</a></li>
             </ul>
-            <a href="" class="linkCart"><i class="fa fa-shopping-bag" aria-hidden="true"></i>(<span class="numbCart">0</span>)</a>
+            <a href="<?php echo APP_URL; ?>cart/" class="linkCart"><i class="fa fa-shopping-bag" aria-hidden="true"></i>(<span class="numbCart">0</span>)</a>
         </div>
         </div>
     </div>
     <div class="headerInner flexBox flexBox--between flexBox--center flexBox--wrap">
     <p id="logo"><a href="<?php echo APP_URL; ?>"><img src="<?php echo APP_URL ?>common/img/header/logo.svg"></a></p>
     <div class="rightHead flexBox flexBox--center flexBox--between">
-        <form class="formSearch" action="<?php echo APP_URL; ?>wp/search/">
+        <form class="formSearch" action="<?php echo APP_URL; ?>search/">
             <div class="wrapSearch">
                 <input type="text" id="tags" name="search" placeholder="Search item...">
                 <button type="submit" class="submitBtn"><img src="<?php echo APP_URL ?>common/img/header/icon_loop@w.svg"></button>
@@ -42,101 +42,180 @@
     </div>
     <nav class="naviWrap pc">
     <ul class="gNavi flexBox flexBox--center flexBox--arround">
-        <li><a href="<?php echo APP_URL; ?>new-arrival">NEW ARRIVAL</a></li>
-        <li><a href="">GOLF CLUBS </a>
-            <ul class="subNavi">
-                <?php
-                    $args=array(
-                    'child_of' => 0,
-                    'orderby' =>'ID',
-                    'order' => 'DESC',
-                    'hide_empty' => 0,
-                    'taxonomy' => 'clubscat',
-                    'number' => '0',
-                    'pad_counts' => false
-                    );
-                    $categories = get_categories($args);
-                    foreach ( $categories as $category ) {
-                    $slug = $category->slug;
-                ?>
-                <li><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i><a href=""><?php echo $category->name; ?></a></li>
-                <?php } ?>
-            </ul>
+        <li><a href="<?php echo APP_URL; ?>new-arrival/">NEW ARRIVAL</a></li>
+        <li><a href="<?php echo APP_URL; ?>sale/">SALE</a></li>
+        <li><a href="<?php echo APP_URL; ?>clubs/">GOLF CLUBS </a>
+            <div class="wrapList">
+                <ul class="subNavi flexBox flexBox--center flexBox--wrap">
+                    <?php
+                        $args=array(
+                        'child_of' => 0,
+                        'orderby' =>'ID',
+                        'order' => 'DESC',
+                        'hide_empty' => 0,
+                        'taxonomy' => 'clubscat',
+                        'number' => '0',
+                        'pad_counts' => false
+                        );
+                        $categories = get_categories($args);
+                        foreach ( $categories as $category ) {
+                        $slug = $category->slug;
+                        $image_cat = wp_get_attachment_image_src(get_field('category_image','clubscat_'.$category->term_id.''),'full');
+
+                    ?>
+                    <li><a href="<?php echo get_term_link($slug,'clubscat');?>">
+                        <?php if($image_cat!='') { ?>
+                            <img src="<?php echo thumbCrop($image_cat[0],70,70) ?>" alt="">
+                        <?php } ?></a> 
+                        <div class="name">
+                            <?php echo $category->name; ?>
+                            <p><a href="<?php echo get_term_link($slug,'clubscat');?>">View more</a></p>
+                        </div>  
+                    </li>
+                    <?php } ?>
+                </ul>
+            </div>
         </li>
-        <li><a href="">GOLF BAGS </a></li>
-        <li><a href="">GOLF SHOES </a>
-            <ul class="subNavi">
-                <?php
-                    $args=array(
-                    'child_of' => 0,
-                    'orderby' =>'ID',
-                    'order' => 'DESC',
-                    'hide_empty' => 0,
-                    'taxonomy' => 'shoescat',
-                    'number' => '0',
-                    'pad_counts' => false
-                    );
-                    $categories = get_categories($args);
-                    foreach ( $categories as $category ) {
-                    $slug = $category->slug;
-                ?>
-                <li><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i><a href=""><?php echo $category->name; ?></a></li>
-                <?php } ?>
-            </ul>
+        <li><a href="<?php echo APP_URL; ?>bag">GOLF BAGS </a>
+            <div class="wrapList">
+                <ul class="subNavi flexBox flexBox--center flexBox--wrap">
+                    <?php
+                        $args=array(
+                        'child_of' => 0,
+                        'orderby' =>'ID',
+                        'order' => 'DESC',
+                        'hide_empty' => 0,
+                        'taxonomy' => 'bagcat',
+                        'number' => '0',
+                        'pad_counts' => false
+                        );
+                        $categories = get_categories($args);
+                        foreach ( $categories as $category ) {
+                        $slug = $category->slug;
+                        $image_cat = wp_get_attachment_image_src(get_field('category_image','bagcat_'.$category->term_id.''),'full');
+
+                    ?>
+                    <li><a href="<?php echo get_term_link($slug,'bagcat');?>">
+                        <?php if($image_cat!='') { ?>
+                            <img src="<?php echo thumbCrop($image_cat[0],70,70) ?>" alt="">
+                        <?php } ?></a> 
+                        <div class="name">
+                            <?php echo $category->name; ?>
+                            <p><a href="<?php echo get_term_link($slug,'bagcat');?>">View more</a></p>
+                        </div>  
+                    </li>
+                    <?php } ?>
+                </ul>
+            </div>
         </li>
-        <li><a href="">APPAREL </a>
-            <ul class="subNavi">
-                <?php
-                    $args=array(
-                    'child_of' => 0,
-                    'orderby' =>'ID',
-                    'order' => 'DESC',
-                    'hide_empty' => 0,
-                    'taxonomy' => 'typecat',
-                    'number' => '0',
-                    'pad_counts' => false
-                    );
-                    $categories = get_categories($args);
-                    foreach ( $categories as $category ) {
-                    $slug = $category->slug;
-                ?>
-                <li><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i><a href=""><?php echo $category->name; ?></a></li>
-                <?php } ?>
-            </ul>
+        <li><a href="<?php echo APP_URL; ?>shoes/">GOLF SHOES</a>
+            <div class="wrapList">
+                <ul class="subNavi flexBox flexBox--center flexBox--wrap">
+                    <?php
+                        $args=array(
+                        'child_of' => 0,
+                        'orderby' =>'ID',
+                        'order' => 'DESC',
+                        'hide_empty' => 0,
+                        'taxonomy' => 'shoescat',
+                        'number' => '0',
+                        'pad_counts' => false
+                        );
+                        $categories = get_categories($args);
+                        foreach ( $categories as $category ) {
+                        $slug = $category->slug;
+                        $image_cat = wp_get_attachment_image_src(get_field('category_image','shoescat_'.$category->term_id.''),'full');
+
+                    ?>
+                    <li><a href="<?php echo get_term_link($slug,'shoescat');?>">
+                        <?php if($image_cat!='') { ?>
+                            <img src="<?php echo thumbCrop($image_cat[0],70,70) ?>" alt="">
+                        <?php } ?></a> 
+                        <div class="name">
+                            <?php echo $category->name; ?>
+                            <p><a href="<?php echo get_term_link($slug,'shoescat');?>">View more</a></p>
+                        </div>  
+                    </li>
+                    <?php } ?>
+                </ul>
+            </div>
         </li>
-        <li><a href="">ACCESSORIES </a>
-            <ul class="subNavi">
-                <?php
-                    $args=array(
-                    'child_of' => 0,
-                    'orderby' =>'ID',
-                    'order' => 'DESC',
-                    'hide_empty' => 0,
-                    'taxonomy' => 'accessoriescat',
-                    'number' => '0',
-                    'pad_counts' => false
-                    );
-                    $categories = get_categories($args);
-                    foreach ( $categories as $category ) {
-                    $slug = $category->slug;
-                ?>
-                <li><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i><a href=""><?php echo $category->name; ?></a></li>
-                <?php } ?>
-            </ul>
+        <li><a href="<?php echo APP_URL; ?>apparel/">APPAREL </a>
+            <div class="wrapList">
+                <ul class="subNavi flexBox flexBox--center flexBox--wrap">
+                    <?php
+                        $args=array(
+                        'child_of' => 0,
+                        'orderby' =>'ID',
+                        'order' => 'DESC',
+                        'hide_empty' => 0,
+                        'taxonomy' => 'typecat',
+                        'number' => '0',
+                        'pad_counts' => false
+                        );
+                        $categories = get_categories($args);
+                        foreach ( $categories as $category ) {
+                        $slug = $category->slug;
+                        $image_cat = wp_get_attachment_image_src(get_field('category_image','typecat_'.$category->term_id.''),'full');
+
+                    ?>
+                    <li><a href="<?php echo get_term_link($slug,'typecat');?>">
+                        <?php if($image_cat!='') { ?>
+                            <img src="<?php echo thumbCrop($image_cat[0],70,70) ?>" alt="">
+                        <?php } ?></a> 
+                        <div class="name">
+                            <?php echo $category->name; ?>
+                            <p><a href="<?php echo get_term_link($slug,'typecat');?>">View more</a></p>
+                        </div>  
+                    </li>
+                    <?php } ?>
+                </ul>
+            </div>
         </li>
-        <li><a href="">CLEARANCE </a></li>
-        <li><a href="<?php echo APP_URL; ?>shop-by-brand">SHOP BY BRAND </a></li>
+        <li><a href="<?php echo APP_URL; ?>accessories/">ACCESSORIES </a>
+            <div class="wrapList">
+                <ul class="subNavi flexBox flexBox--center flexBox--wrap">
+                    <?php
+                        $args=array(
+                        'child_of' => 0,
+                        'orderby' =>'ID',
+                        'order' => 'DESC',
+                        'hide_empty' => 0,
+                        'taxonomy' => 'accessoriescat',
+                        'number' => '0',
+                        'pad_counts' => false
+                        );
+                        $categories = get_categories($args);
+                        foreach ( $categories as $category ) {
+                        $slug = $category->slug;
+                        $image_cat = wp_get_attachment_image_src(get_field('category_image','accessoriescat_'.$category->term_id.''),'full');
+
+                    ?>
+                    <li><a href="<?php echo get_term_link($slug,'accessoriescat');?>">
+                        <?php if($image_cat!='') { ?>
+                            <img src="<?php echo thumbCrop($image_cat[0],70,70) ?>" alt="">
+                        <?php } ?></a> 
+                        <div class="name">
+                            <?php echo $category->name; ?>
+                            <p><a href="<?php echo get_term_link($slug,'accessoriescat');?>">View more</a></p>
+                        </div>  
+                    </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </li>
+        <li><a href="<?php echo APP_URL; ?>brand/">SHOP BY BRAND </a></li>
         <li><a href="<?php echo APP_URL; ?>event">EVENT</a></li>
-        <li><a href="<?php echo APP_URL; ?>sale">SALE</a></li>
+        
     </ul>
 </nav>
 </header>
 
 <div class="subMenu">
-        <ul class="langBar">
-            <li class="active"><a href="">English</a></li>
-            <li><a href="">Vietnamese</a></li>
-        </ul>
+    <!-- <ul class="langBar">
+        <li class="active"><a href="">English</a></li>
+        <li><a href="">Vietnamese</a></li>
+    </ul> -->
     <ul class="menuList">
         <li><a href="">NEW ARRIVAL</a></li>
         <li><a href="">GOLF CLUBS </a></li>
--- libs/sidebar.php
+++ libs/sidebar.php
@@ -1,37 +1,41 @@
 <aside class="leftSide">
-			<h3 class="h3_shop">Shop by type</h3>
-				<ul class="lstFilter">
-					<?php
-						$args=array(
-							'child_of' => 0,
-							'orderby' =>'ID',
-							'order' => 'DESC',
-							'hide_empty' => 0,
-							'taxonomy' => $cateType,
-							'number' => '0',
-							'pad_counts' => false
-							);
-							$categories = get_categories($args);
-							foreach ( $categories as $category ):
-							$slug = $category->slug;
-					?>
-					<li><a href="<?php echo get_term_link($slug,$cateType);?>"><?php echo $category->name;  ?></a></li>
-					<?php endforeach; ?>
-				</ul>
-			<h3 class="h3_shop">Shop by Brand</h3>
-			<ul class="lstFilter__brand">
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo1.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo2.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo3.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo4.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo5.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo6.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo7.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo8.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo9.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo10.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo11.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo12.jpg" class="" alt=""></a></li>
-				<li><a href=""><img src="<?php echo APP_URL; ?>img/top/logo13.jpg" class="" alt=""></a></li>
-			</ul>
-		</aside>
+	<h3 class="h3_shop">Shop by type</h3>
+		<ul class="lstFilter">
+			<?php
+				$args=array(
+					'child_of' => 0,
+					'orderby' =>'ID',
+					'order' => 'DESC',
+					'hide_empty' => 0,
+					'taxonomy' => $cateType,
+					'number' => '0',
+					'pad_counts' => false
+					);
+					$categories = get_categories($args);
+					foreach ( $categories as $category ):
+					$slug = $category->slug;
+			?>
+			<li><a href="<?php echo get_term_link($slug,$cateType);?>"><?php echo $category->name;  ?></a></li>
+			<?php endforeach; ?>
+		</ul>
+	<h3 class="h3_shop">Shop by Brand</h3>
+	<ul class="lstFilter__brand">
+		<?php
+			$wp_query = new WP_Query();
+			$param = array (
+			'posts_per_page' => '-1',
+			'post_type' => 'brand',
+			'post_status' => 'publish',
+			'order' => 'DESC',
+			'paged' => $paged,
+			);
+			$wp_query->query($param);
+			if($wp_query->have_posts()): while($wp_query->have_posts()) :$wp_query->the_post();
+			$thumb_img = get_post_thumbnail_id($post->ID);
+			$thumb_url = wp_get_attachment_image_src($thumb_img,'full');
+		?>
+		<li><a href="<?php echo APP_URL; ?>brand/?brand=<?php echo $post->post_name; ?>"><img src="<?php echo $thumb_url[0]; ?>" class="" alt=""></a></li>
+		<?php endwhile;endif; ?>
+	</ul>
+</aside>
+<?php wp_reset_query() ?>
